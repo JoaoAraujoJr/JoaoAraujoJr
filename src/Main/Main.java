cab93176e664df3a0117f5725ee5eea1bb5e4772
Joao <p191@fct.unl.pt>
2023-09-19 18:23:24 +0100

    segunda versao

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,6 +6,6 @@ package Main;
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Animal git demo2");
+        System.out.println("Animal git demo2 melhorado");
     }
 }
